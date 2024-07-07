b5cf398854c237df2f0e2f20ce34fa23d474b05a
developer-tamim <tawsiftamim24@gmail.com>
2024-07-07 22:44:55 +0600

    completed

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
-new VTTCue({
+new Vue({
     el: '#app',
     data: {
         newTask: '',
         tasks: []
     },
     methods: {
-        addTask(){
-            if(this.newTask.trim() !== '') {
+        addTask() {
+            if (this.newTask.trim() !== '') {
                 this.tasks.push({
                     text: this.newTask.trim(),
                     completed: false,
-                    date: new Date().toLocaleDateString() 
+                    date: new Date().toLocaleDateString()
                 });
                 this.newTask = '';
             }
         },
-        removeTask(index){
+        removeTask(index) {
             this.tasks.splice(index, 1);
         },
-        toggleTaskStatus(index){
-            this.tasks[index].completed = !this.tasks[index].completed;
-        },
-        clearCompletedTasks(){
-            this.tasks = this.tasks.filter(task =>!task.completed);
+        clearCompletedTasks() {
+            this.tasks = this.tasks.filter(task => !task.completed);
         }
-    },
-})
+    }
+});
